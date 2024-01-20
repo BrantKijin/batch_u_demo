5a0abee1bd442ad60c1b7cb6e627fae7bc6ec611
곽기진 <upper0house@gmail.com>
2024-01-20 17:17:13 +0900

    feat: step 추가 예제

--- src/main/java/org/example/batch_u_demo/config/BatchConfiguration.java
+++ src/main/java/org/example/batch_u_demo/config/BatchConfiguration.java
@@ -21,7 +21,7 @@ public class BatchConfiguration {
 	public final StepBuilderFactory stepBuilderFactory;
 
 	@Bean
-	public Step firstStep(){
+	public Step step1(){
 		return this.stepBuilderFactory.get("step1")
 			.tasklet(new Tasklet() {
 				@Override
@@ -36,9 +36,40 @@ public class BatchConfiguration {
 	}
 
 	@Bean
+	public Step step2(){
+		return this.stepBuilderFactory.get("step2")
+			.tasklet(new Tasklet() {
+				@Override
+				public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws
+					Exception {
+					System.out.println("step1 executed!!");
+					return RepeatStatus.FINISHED;
+				}
+			})
+			.build();
+
+	}
+	@Bean
+	public Step step3(){
+		return this.stepBuilderFactory.get("step3")
+			.tasklet(new Tasklet() {
+				@Override
+				public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws
+					Exception {
+					System.out.println("step1 executed!!");
+					return RepeatStatus.FINISHED;
+				}
+			})
+			.build();
+	}
+
+
+	@Bean
 	public Job firstJob(){
 		return this.jobBuilderFactory.get("job1")
-			.start(firstStep())
+			.start(step1())
+			.next(step2())
+			.next(step3())
 			.build();
 	}
 }
