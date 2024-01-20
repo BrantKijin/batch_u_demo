b3d856c0746595c7a5aec7fadc1bd21345dbecfc
곽기진 <upper0house@gmail.com>
2024-01-20 15:23:23 +0900

    init: 초기

---
+++ src/test/java/org/example/batch_u_demo/BatchUDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.batch_u_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchUDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
